cef763e7a00477405c8b0f422410aa268a8b5aeb
Marcio Araujo <marcio.araujo.m7@gmail.com>
2023-03-05 13:55:11 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <VueComments />
+</template>
+
+<script>
+import VueComments from './components/VueComments.vue';
+
+export default {
+  name: 'App',
+  components: {
+    VueComments
+  }
+}
+</script>
---
+++ src/components/VueComments.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ name }}</div>
+</template>
+
+<script>
+
+export default {
+  name: "VueComments",
+  data() {
+    return {
+      name: "Marcio"
+    }
+  }
+
+}
+</script>
